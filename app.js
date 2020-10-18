85d3f666d15a68d2b87d620cfdf5f1da74910077
praveeno <ps1995.ps@gmail.com>
2020-10-18 09:47:22 +0530

    feat: game over window, restart, fix win logic

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ const info1 = document.getElementById('player-info1');
 const info2 = document.getElementById('player-info2');
 const colors1 = document.getElementById('colors');
 const colors2 = document.getElementById('colors2');
+const scoreXElem = document.getElementById('point1');
+const scoreOElem = document.getElementById('point2');
+const msgWindow = document.getElementById('msg-window');
+const wrapper = document.getElementById('wrapper');
 
+let scoreO = 0;
+let scoreX = 0;
+let gamePause = false;
+// const player1Name = prompt("Player 1 name");
+// const player2Name = prompt("Player 2 name");
 function addColorTab(elem, whoToUpdate) {
   const colors = ['red', 'green', 'blue', 'orange', 'purple', 'pink'];
 
@@ -36,14 +45,14 @@ function addColorTab(elem, whoToUpdate) {
 addColorTab(colors1, 'player1');
 addColorTab(colors2, 'player2');
 
-const gameGrid = [
+let gameGrid = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
 ]
 
 function onGameSectionSelect(elem) {
-  if (!this.innerHTML) {
+  if (!this.innerHTML && !gamePause) {
     gameGrid[elem.target.parentElement.id][elem.target.id] = currentPlayer;
 
     if (currentPlayer == playerX) {
@@ -56,10 +65,58 @@ function onGameSectionSelect(elem) {
     checkWhoseDiiiya();
 
     const winner = geGameWinner();
-
     if (winner !== null) {
-      alert(winner);
+      gamePause = true;
+      if (winner == playerO) {
+        scoreO++;
+        scoreOElem.innerHTML = `Points: ${scoreO}`;
+        askToRestart(`Player 1 (${playerO}) Won, Restart ?`);
+      } else if (winner == playerX) {
+        scoreX++;
+        scoreXElem.innerHTML = `Points: ${scoreX}`;
+        askToRestart(`Player 1 (${playerX}) Won, Restart ?`);
+      } else if (winner == 'draw') {
+        askToRestart("Game Graw, Restart ?");
+      }
     }
+  }
+}
+
+function askToRestart(msg) {
+  wrapper.style.filter = 'blur(5px)';
+  msgWindow.innerHTML = `
+    <div>
+      <div>
+        ${msg}
+      </div>
+      <div>
+      Player 1 Score ${scoreX}
+      </div>
+      <div>
+      Player 2 Score ${scoreO}
+      </div>
+      <div>
+      </div>
+      <div>
+        <button id="restartButton">Restart</button>
+      </div>
+    </div>
+  `
+  document.getElementById("restartButton").addEventListener("click", onRestart);
+  function onRestart() {
+    msgWindow.innerHTML = null;
+    wrapper.style.filter = '';
+    currentPlayer = playerX;
+    gamePause = false;
+    gameGrid = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ];
+    Array.from(allTd).forEach(td => {
+      td.innerHTML = null;
+      td.classList.remove("shadow");
+    });
   }
 }
 
@@ -97,16 +154,19 @@ function geGameWinner() {
       return gameGrid[0][i];
     }
     //column win
-    if (notBlank(gameGrid[0][i]) &&gameGrid[0][i] == gameGrid[1][i] && gameGrid[1][i] == gameGrid[2][i]) {
+    if (notBlank(gameGrid[0][i]) && gameGrid[0][i] == gameGrid[1][i]
+      && gameGrid[1][i] == gameGrid[2][i]) {
       return gameGrid[0][i];
     }
   }
   //diagonal win
-  if (notBlank(gameGrid[0][0]) && gameGrid[0][0] == gameGrid[1][1] && gameGrid[1][1] == gameGrid[2][2]) {
+  if (notBlank(gameGrid[0][0]) && gameGrid[0][0] == gameGrid[1][1]
+    && gameGrid[1][1] == gameGrid[2][2]) {
     return gameGrid[0][0];
   }
-  if (notBlank(gameGrid[2][0]) && gameGrid[2][0] == gameGrid[1][1] && gameGrid[1][1] == gameGrid[0][2]) {
-    return gameGrid[0][0];
+  if (notBlank(gameGrid[2][0]) && gameGrid[2][0] == gameGrid[1][1]
+    && gameGrid[1][1] == gameGrid[0][2]) {
+    return gameGrid[2][0];
   }
 
   // draw
@@ -123,8 +183,4 @@ function geGameWinner() {
 
 function notBlank(block) {
   return block !== null;
-}
-
-function rowWin(row) {
-  return  row[0] === row[1] && row[1] === row[2] ? row[0]: null;
 }
